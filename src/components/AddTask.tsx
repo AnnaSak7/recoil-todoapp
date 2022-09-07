e3fdeeac5b2a70325d591bfd973f348ffd05d9c6
Anna Sakurai <zenasak@gmail.com>
2022-09-07 20:52:10 +0200

    complete

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { addTitleState, addTitleStateLength } from "../states/addTitleState";
 import "./AddTask.css";
 
 const AddTask = () => {
+  const addTitle = useRecoilValue(addTitleState);
+  const addTitleLength = useRecoilValue(addTitleStateLength);
   return (
     <div className="taskField">
-      <div>5 tasks</div>
+      <div>{addTitleLength} tasks</div>
       <ul>
-        <li>First task</li>
+        {addTitle.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
       </ul>
     </div>
   );
--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -2,16 +2,22 @@ import React, { useCallback } from "react";
 import "./InputTask.css";
 import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { inputTitleState } from "../states/inputTitleState";
+import { addTitleState } from "../states/addTitleState";
+
+const getKey = () => Math.random().toString(32).substring(2);
 
 const InputTask = () => {
   const [inputTitle, setInputTitle] = useRecoilState(inputTitleState);
-
   // OR
   //   const inputTitle = useRecoilValue(inputTitleState);
   //   const setInputTitle = useSetRecoilState(inputTitleState);
 
+  const [addTitle, setAddTitle] = useRecoilState(addTitleState);
+
   const handleClick = () => {
-    console.log(inputTitle);
+    setAddTitle([...addTitle, { id: getKey(), title: inputTitle }]);
+    setInputTitle("");
+    console.log(addTitle);
   };
 
   const onChange = useCallback(
@@ -24,7 +30,12 @@ const InputTask = () => {
 
   return (
     <div className="inputField">
-      <input type="text" className="inputTitle" onChange={onChange} />
+      <input
+        type="text"
+        className="inputTitle"
+        onChange={onChange}
+        value={inputTitle}
+      />
       <button type="button" className="addButton" onClick={handleClick}>
         Add
       </button>
