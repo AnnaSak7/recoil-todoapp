f2bf3f289a4e41b18e589216b8d7cae8b0fc1c30
Anna Sakurai <zenasak@gmail.com>
2022-09-07 18:43:41 +0200

    Add states directory. Add function to get the input value.

---
+++ README.md
@@ -0,0 +1,9 @@
+npm create vite
+
+npm i
+
+npm i recoil @types/recoil
+
+- mkdir states
+- touch states/inputTitleState.ts
+- set ##<RecoilRoot>## where you want to use the states
--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./InputTask.css";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { inputTitleState } from "../states/inputTitleState";
 
 const InputTask = () => {
+  const [inputTitle, setInputTitle] = useRecoilState(inputTitleState);
+
+  // OR
+  //   const inputTitle = useRecoilValue(inputTitleState);
+  //   const setInputTitle = useSetRecoilState(inputTitleState);
+
+  const handleClick = () => {
+    console.log(inputTitle);
+  };
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputTitle(e.target.value);
+      console.log(inputTitle);
+    },
+    [inputTitle]
+  );
+
   return (
     <div className="inputField">
-      <input type="text" className="inputTitle" />
-      <button type="button" className="addButton">
+      <input type="text" className="inputTitle" onChange={onChange} />
+      <button type="button" className="addButton" onClick={handleClick}>
         Add
       </button>
     </div>
